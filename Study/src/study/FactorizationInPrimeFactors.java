5889e488c88844ddd36f7d60c96ba529f787d745
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-05-29 20:49:38 +0900

    백준 11653번 소인수분해

--- Study/src/study/FactorizationInPrimeFactors.java
+++ Study/src/study/FactorizationInPrimeFactors.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class FactorizationInPrimeFactors {		//백준 11653번 소인수분해
-
+												// 제출할 땐 Main으로 바꿀 것.
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
