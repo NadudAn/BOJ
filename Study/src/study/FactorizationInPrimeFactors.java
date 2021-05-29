fd554a486f67f61c2e6640e998308dbf3f19f9ce
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-05-29 20:45:01 +0900

    백준 11653번 소인수분해

---
+++ Study/src/study/FactorizationInPrimeFactors.java
@@ -0,0 +1,24 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class FactorizationInPrimeFactors {		//백준 11653번 소인수분해
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine()), cnt = 2;
+		
+		while(true) {
+			if(n % cnt == 0) {
+				n /= cnt;
+				System.out.println(cnt);
+				cnt = 2;
+			}
+			else ++cnt;
+			if(n <= 1) break;
+		}
+	}
+}
