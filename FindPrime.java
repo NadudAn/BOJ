c98cdee16cba10beab0ea365b644c1c975bf92de
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-05-31 18:18:32 +0900

    [백준] 1929 : 소수 구하기
    문제
    M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
    
    ​출력
    한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.

---
+++ FindPrime.java
@@ -0,0 +1,31 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class FindPrime {				//백준 1929번 소수 구하기
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		for(int i = n; i <= m; i++) {
+			if(IsPrime(i)) System.out.println(i);
+		}
+	}
+	
+	public static boolean IsPrime(int n) {
+		if(n <= 1) return false;
+
+		for(int i = 2; i <= Math.sqrt(n); i++) 
+			if(n % i == 0) return false;
+
+		return true;
+	}
+
+}
