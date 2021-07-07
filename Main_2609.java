3be7028dd78ebc3692cfa7d7a07f5cb36d1167f9
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-07-07 23:54:56 +0900

    [백준] 2609 : 최대공약수와 최소공배수
    문제
    두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에는 두 개의 자연수가 주어진다. 이 둘은 10,000이하의 자연수이며 사이에 한 칸의 공백이 주어진다.
    
    출력
    첫째 줄에는 입력으로 주어진 두 수의 최대공약수를, 둘째 줄에는 입력으로 주어진 두 수의 최소 공배수를 출력한다.

---
+++ Main_2609.java
@@ -0,0 +1,33 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2609 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int gcd = gcd(a, b);
+		
+		System.out.println(gcd);
+		System.out.println(a * b / gcd);
+	}
+	
+	static int gcd(int a, int b) {
+		int r;
+		
+		while(b != 0) {
+			r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+
+}
