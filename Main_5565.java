ac54f8b912144eaa59da162f7ecf37cbcb703ada
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-07-28 15:46:24 +0900

    [백준] 5565 : 영수증
    문제
    새 학기를 맞아 상근이는 책을 10권 구입했다. 상근이는 의욕이 너무 앞서서 가격을 조사하지 않고 책을 구입했다. 이제 각 책의 가격을 알아보려고 한다.
    하지만, 영수증에는 얼룩이 묻어있었고, 상근이는 책 10권 중 9권의 가격만 읽을 수 있었다.
    책 10권의 총 가격과 가격을 읽을 수 있는 9권 가격이 주어졌을 때, 가격을 읽을 수 없는 책의 가격을 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 10권의 총 가격이 주어진다. 둘째 줄부터 9개 줄에는 가격을 읽을 수 있는 책 9권의 가격이 주어진다. 책의 가격은 10,000이하인 양의 정수이다.
    
    출력
    첫째 줄에 가격을 읽을 수 없는 책의 가격을 출력한다.

---
+++ Main_5565.java
@@ -0,0 +1,20 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_5565 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < 9; i++) 
+			n -= Integer.parseInt(br.readLine());
+		
+		System.out.println(n);
+
+	}
+
+}
