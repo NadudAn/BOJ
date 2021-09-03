b05d2fa0847e89b98bf4506b01c2c5e9edc6e200
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-09-03 23:34:36 +0900

    [백준] 10998 : A x B
    문제
    두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
    
    출력
    첫째 줄에 A×B를 출력한다.

---
+++ Main_10998.java
@@ -0,0 +1,20 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_10998 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		n *= Integer.parseInt(st.nextToken());
+		System.out.println(n);
+		
+	}
+
+}
