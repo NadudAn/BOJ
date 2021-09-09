484e1d5a8320e0093518e836463d9af7caf22b91
NadudAn <84761609+NadudAn@users.noreply.github.com>
2021-09-09 18:51:34 +0900

    [백준] 10171 : 고양이
    문제
    아래 예제와 같이 고양이를 출력하시오.
    
    입력
    없음.
    
    출력
    고양이를 출력한다.

---
+++ Main_10171.java
@@ -0,0 +1,13 @@
+package study2;
+
+public class Main_10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+
+}
