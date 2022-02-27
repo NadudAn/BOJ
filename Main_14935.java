83a5c66d5899df45188ac748f92f3d59ad784b74
Nayeong An <dsd932@naver.com>
2022-02-27 22:00:42 +0900

    [백준] 14935 : FA
    문제
    함수 F(x)는 입력으로 주어진 수 x의 첫 자리와 수 x의 자리수를 곱한 결과를 반환하는 함수이다.
    예를 들어 x = 932 일때 F(x)는 9×3으로 27을 반환한다.
    입력받은 x에 대해서 함수 F를 수행하고, 나온 결과값에 다시 함수 F를 수행하는 것을 반복한다. 계속 반복해서 수행했을 때 어느 시점에서부터 동일한 수가 나오는 경우, 입력 x를 FA수 라고 한다.
    입력 x가 주어졌을때 이 수가 FA 수인지 출력하라.
    
    입력
    정수 x 가 주어진다. (0 ≤ x ≤ 10100)
    
    출력
    정수 x가 FA수 라면 FA를 출력하고, 아니라면 NFA를 출력한다.
    
    
    수학적으로 접근하면 NFA인 수가 없습니다.

---
+++ Main_14935.java
@@ -0,0 +1,16 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_14935 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		System.out.println("FA");
+
+	}
+
+}
