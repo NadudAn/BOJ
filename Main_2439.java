3e8bcd9220bd0658bd1991e015100719a1a849a2
Nayeong An <dsd932@naver.com>
2022-03-09 00:06:37 +0900

    [백준] 2439 : 별 찍기 - 2
    문제
    첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
    하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
    
    입력
    첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
    
    출력
    첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.

---
+++ Main_2439.java
@@ -0,0 +1,23 @@
+package study2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2439 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = n; j >= 1; j--) {
+				if(j > i) System.out.print(" ");
+				else System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
